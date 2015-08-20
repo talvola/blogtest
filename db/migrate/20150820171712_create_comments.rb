17c32b60cdaf8253ce8d085f51d06503382bbf0e
Erik Talvola <talvola@gmail.com>
2015-08-20 10:51:28 -0700

    step 6.1

--- db/migrate/20150820171712_create_comments.rb
+++ db/migrate/20150820171712_create_comments.rb
@@ -1,7 +1,7 @@
 class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
-      t.String :commenter
+      t.string :commenter
       t.text :body
       t.references :article, index: true, foreign_key: true
 
